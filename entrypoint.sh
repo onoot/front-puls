4719386bd53c62eec97202fee73bf096f1f03bbb
onoot <penisxuei@email.com>
2026-07-14 21:19:10 +0700

    fix: default BACKEND_URL in entrypoint if not set

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+export BACKEND_URL="${BACKEND_URL:-http://localhost:3000}"
 envsubst '${BACKEND_URL}' < /etc/nginx/conf.d/default.conf > /etc/nginx/conf.d/default.conf.tmp
 mv /etc/nginx/conf.d/default.conf.tmp /etc/nginx/conf.d/default.conf
 exec supervisord -c /etc/supervisord.conf
